8e8a9b81f7f2ccc894f53e46a598547818ee48d4
Vinit Savani <136707852+VinitSavani@users.noreply.github.com>
2023-08-16 18:04:39 +0530

    Add files via upload

---
+++ vin_pr_Q2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class time
+{
+	public:
+		int hor;
+		int min;
+		int sec;
+};
+
+int main()
+{
+	time v1,v2,v3;
+	
+	cout << endl << "Enter First Velue :- " << endl;
+	cout << "Hour :- " ;
+	cin >> v1.hor;
+	cout << "Minute :- " ;
+	cin >> v1.min;
+	cout << "Secend :- " ;
+	cin >> v1.sec;
+	cout << endl << "Enter Second Velue :- " << endl;
+	cout << "Hour :- " ;
+	cin >> v2.hor;
+	cout << "Minute :- " ;
+	cin >> v2.min;
+	cout << "Secend :- " ;
+	cin >> v2.sec;
+	
+	v1.hor =
+	v2.
+	return 0;
+}
